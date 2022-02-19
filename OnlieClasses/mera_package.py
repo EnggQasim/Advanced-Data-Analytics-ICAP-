a05847891794a72ad99c41e855086f577d40cf5f
Muhammad Qasim <m.qasim077@gmail.com>
2022-02-19 22:03:02 +0500

    online class8

---
+++ OnlieClasses/mera_package.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    return f"Hello Mr/Miss {name}. Welcome in online session"
+
+def agenda_meeting(agenda):
+    return f"{agenda} we will cover it."
+
+owner_name = "Nasir Hussain"
+class_name = "class8"
